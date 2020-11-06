17f616d01573f923b8ef9167df39a9e2cce00c1c
perfr <per.fredelius@gmail.com>
2020-11-06 21:57:51 +0100

    WIP experiments

---
+++ Readme.md
@@ -0,0 +1,17 @@
+
+
+# Setup
+
+## Download and install Open Modelica
+
+https://openmodelica.org/
+
+## Install OMPython
+
+https://github.com/OpenModelica/OMPython
+
+# Run
+
+```bash
+$ python main.py
+```
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import os
 from OMPython import OMCSessionZMQ
+from OMPython import ModelicaSystem
+os.chdir("output")
 omc = OMCSessionZMQ()
-cmds = [
-  'loadFile(getInstallationDirectoryPath() + "/share/doc/omc/testmodels/BouncingBall.mo")',
-  "simulate(BouncingBall, stopTime=3.0)",
-  "plot(h)"
-  ]
-for cmd in cmds:
-  answer = omc.sendExpression(cmd)
-  print("\n{}:\n{}".format(cmd, answer))
+model_path=omc.sendExpression("getInstallationDirectoryPath()") + "/share/doc/omc/testmodels/"
+mod=ModelicaSystem(model_path + "BouncingBall.mo","BouncingBall",["Modelica"])
+#mod.buildModel()
+print(mod.simulate()) # (simflags="stopTime=3.0")
+mod.setLinearizationOptions(["stopTime=2.0"])
+print(mod.linearize())
+#outp = mod.linearize() # mod.getOutputs()
+#print(outp)
+# cmds = [
+#   'loadFile(getInstallationDirectoryPath() + "/share/doc/omc/testmodels/BouncingBall.mo")',
+#   "simulate(BouncingBall, stopTime=3.0)",
+#   "plot(h)"
+#   ]
+# for cmd in cmds:
+#   answer = omc.sendExpression(cmd)
+#   print("\n{}:\n{}".format(cmd, answer))
