1bd758dbb9e6fdc9f85a5ad5b2dc1791b9a71028
perfr <per.fredelius@gmail.com>
2020-11-13 23:26:13 +0100

    WIP docker conf

--- commit-and-deploy.sh
+++ commit-and-deploy.sh
@@ -1,4 +1,6 @@
 set -e
 
+git add .
+git commit --reuse-message=HEAD
 git push
 ssh root@165.227.168.170 '(cd openmodelica-graphql/docker-compose && git pull && docker-compose up -d --force-recreate) ; exit'
