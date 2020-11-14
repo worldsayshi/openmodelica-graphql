200c2782a74ca61cf756ca97d48da8812372b328
perfr <per.fredelius@gmail.com>
2020-11-14 18:20:25 +0100

    Added problematic installog

--- commit-and-deploy.sh
+++ commit-and-deploy.sh
@@ -3,4 +3,4 @@ set -e
 git add .
 git commit --reuse-message=HEAD
 git push
-ssh root@165.227.168.170 '(cd openmodelica-graphql/docker-compose && git pull && docker-compose up -d --force-recreate) ; exit'
+source deploy.sh
---
+++ deploy.sh
@@ -0,0 +1,2 @@
+
+ssh root@165.227.168.170 '(cd openmodelica-graphql/docker-compose && git pull && docker-compose up -d --force-recreate) ; exit'
